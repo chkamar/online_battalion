428f43a89a3cd289d35b139ecfb1505ce56a0c38
chkamar <1714538618@qq.com>
2019-06-15 13:06:45 +0800

    开启redis缓存注解

--- src/main/java/cn/ch/battalion/BattalionApplication.java
+++ src/main/java/cn/ch/battalion/BattalionApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableTransactionManagement
+@EnableCaching
 @MapperScan("cn.ch.battalion.core.dao")
 public class BattalionApplication {
 
